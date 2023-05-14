effedaa8b983b36dd6dfa123f0ab5ffd29ab8a17
Jasper Bok <hello@jasperbok.nl>
2023-05-14 09:35:26 +0200

    Add /recipe handling

--- main.go
+++ main.go
@@ -11,6 +11,26 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
+func handleYesBtn(c tele.Context) error {
+	responses := []string{
+		"%s zegt in ieder geval 'ja'",
+		"Da's een 'ja' van %s",
+		"%s is akkoord",
+		"%s zegt 'ja'",
+	}
+	return c.Send(fmt.Sprintf(responses[rand.Intn(len(responses))], c.Sender().Username))
+}
+
+func handleNoBtn(c tele.Context) error {
+	responses := []string{
+		"%s wilt het niet",
+		"%s zegt 'neen'",
+		"Geen akkoord van %s",
+		"Van %s moet ik wat anders bedenken",
+	}
+	return c.Send(fmt.Sprintf(responses[rand.Intn(len(responses))], c.Sender().Username))
+}
+
 func main() {
 	pref := tele.Settings{
 		Token:  os.Getenv("TELEGRAM_BOT_TOKEN"),
@@ -23,24 +43,32 @@ func main() {
 		return
 	}
 
-	b.Handle("/hello", func(c tele.Context) error {
-		return c.Send("Hello!")
+	b.Handle("/recept", func(c tele.Context) error {
+		// Universal markup builders.
+		selector := &tele.ReplyMarkup{}
+
+		// Inline buttons.
+		inYes := selector.Data("Aw yis", "yes")
+		inNo := selector.Data("Nooooz", "no")
+
+		selector.Inline(selector.Row(inYes, inNo))
+
+		b.Handle(&inYes, handleYesBtn)
+		b.Handle(&inNo, handleNoBtn)
+
+		recipes := GetRecipes()
+		choice := recipes[rand.Intn(len(recipes))]
+		return c.Send(fmt.Sprintf("Wat dacht je van %s?", strings.ToLower(choice.Title)), selector)
 	})
 
-	b.Handle(tele.OnText, handleRecipe)
+	b.Handle(tele.OnText, handleText)
 
 	b.Start()
 }
 
-func handleRecipe(c tele.Context) error {
+func handleText(c tele.Context) error {
 	user := c.Sender()
 	text := c.Text()
-
-	if strings.ToLower(text) == "recept" {
-		recipes := GetRecipes()
-		choice := recipes[rand.Intn(len(recipes))]
-		return c.Send(fmt.Sprintf("Wat dacht je van %s?", strings.ToLower(choice.Title)))
-	}
 
 	return c.Send(fmt.Sprintf("%s zei '%s'", user.Username, text))
 }
